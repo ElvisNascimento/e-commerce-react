fc6895f57b13c29f9f2d66724aa07ce9e0a71a7a
SaorCampos <saor93@gmail.com>
2022-10-11 19:07:08 -0300

    instalado todas a bibliotecas que vão ser usadas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import Produtos from "./components/Produtos/index.js";
 import React, { useState } from "react";
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Header from "./components/Header/index.js";
 
 export const AppContext = React.createContext();
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '../node_modules/bootstrap/dist/js/bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import { BrowserRouter } from 'react-router-dom';
 import "./css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
