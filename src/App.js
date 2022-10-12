9d75d85d2502c435a20c80d1e1e1a3d9d91cc9bd
mariolivera <mariooliveiratec@hotmail.com>
2022-10-12 20:32:23 -0300

    criacao de todos os links e linkando as paginas de produto e linkando ao appjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // import Produtos from "./components/Produtos/index.js";
 import React, { useState } from "react";
-// import {Routes, Route, Navigate} from 'react-router-dom'
+//import {Routes, Route, Navigate} from 'react-router-dom';
 import Header from "./components/Header/index.js";
 import Collection from "./components/Produto/Collection/index.js";
+// import Linkicons from "./componets/Produto/Collection/Linkicons"
+// import Bonés from "./components/Produtos/Bonés";
+// import Calças from "./components/Produtos/Calças";
+// import Camisetas from "./components/Produtos/Camisetas";
+// import Headphones from "./components/Produtos/Headphones";
+// import Tênis from "./components/Produtos/Tênis";
+
 
 export const AppContext = React.createContext();
 
@@ -16,8 +23,21 @@ export default function App() {
         {/* <Produtos /> */}
         <Collection/>
       </>
-    </AppContext.Provider>
+        </AppContext.Provider>
+        
+        /* <BrowserRouter>
+        //   <Linkicons/>
+        //       <Routes>
+        //         <Route path="/Camisetas" element={<Camisetas/>}/>
+        //         <Route path="/Calças" element={<Calças/>}/>
+        //         <Route path="/Bonês" element={<Bonés/>}/>
+        //         <Route path="/Headphone" element={<Headphones/>}/>
+        //         <Route path="/Ténis" element={<Tênis/>}/>
+        //       </Routes>
+        //   </BrowserRouter>*/
+        
   )
+  
 }
 
 
--- src/components/Produto/Collection/Linkicons/Linkicons.js
+++ src/components/Produto/Collection/Linkicons/Linkicons.js
@@ -1,17 +1,27 @@
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import React from "react";
+// import { Link, useLocation } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.css";
+// import React from "react";
 
-export default function Linkicons(){
-   
-    return(
 
-        <nav>
-            <Link to= "/Camisetas" className = {'/Camisetas'}>Camisetas</Link>
-            <Link to= "/Calças" className = {'/Calças'}>Calças</Link>
-            <Link to= "/Bonés" className = {'/Bonés'}>Bonés</Link>
-            <Link to= "/Headphones" className = {'/Headphones'}>Headphones</Link>
-            <Link to= "/Tênis" className = {'/Tênis'}>Tênis</Link>
-        </nav>
-    )
-}
+// export default function Linkicons(){
+//     const location = useLocation();
+
+//     const ativar = url => {
+//         if ( url === location.pathname){
+//             return "btn btn-primary";
+//         }
+//         return "btn btn-outline-primary";
+//     }
+//     return(
+//         <div>
+//             <p>Coleções em destaque</p>
+//             <nav>
+//                 <Link to= "/Camisetas" className = {ativar('/Camisetas')}>Camisetas</Link>
+//                 <Link to= "/Calças" className = {ativar('/Calças')}>Calças</Link>
+//                 <Link to= "/Bonés" className = {ativar('/Bonés')}>Bonés</Link>
+//                 <Link to= "/Headphones" className = {ativar('/Headphones')}>Headphones</Link>
+//                 <Link to= "/Tênis" className = {ativar('/Tênis')}>Tênis</Link>
+//             </nav>
+//         </div>
+//     );
+// }
