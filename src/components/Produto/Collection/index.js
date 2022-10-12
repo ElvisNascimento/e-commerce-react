5b6b103fec398a66771fd2b8b87fda23345f5b2c
mariolivera <mariooliveiratec@hotmail.com>
2022-10-12 12:37:33 -0300

    atualizando

--- src/components/Produto/Collection/index.js
+++ src/components/Produto/Collection/index.js
@@ -1,9 +1,6 @@
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-export default function Colletion(){
+export default function Colletion() {
     return(
         <div>
             <span>Coleção em destaque<span/>
