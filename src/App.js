35a68935b6f52cf50832d3e28c22455d4b8d789d
carloeira <augustovieira.cb@gmail.com>
2022-10-12 12:21:22 -0300

    Ajustei um container para a Collection

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
       <>
         <Header />
         {/* <Produtos /> */}
-        {Collection}
+        <Collection/>
       </>
     </AppContext.Provider>
   )
--- src/components/Produto/Collection/index.js
+++ src/components/Produto/Collection/index.js
@@ -1,11 +1,16 @@
 
+import { CardContent } from '@mui/material';
+import { Container } from '@mui/system';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Colletion() {
     return(
-        <div>
-            <p>Coleção em destaque</p>
-        </div>
+        <section>
+            <Container className="container-md mt-2 border">
+            <p>Coleções em destaque</p>
+            <CardContent></CardContent>
+            </Container>
+        </section>
     )
 }
