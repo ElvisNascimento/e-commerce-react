55fa8da7ba1f4ac7df5f0841acd3b4a63164560c
adriellysoares <AdriellySoares>
2022-10-13 11:43:03 -0300

    atualizando

---
+++ src/components/banner-footer/index.js
@@ -0,0 +1,41 @@
+import {  } from "module";
+import'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function bannerFooter() {
+    return (
+
+        <section>
+<div class="r2">
+    <div class="r21">
+        <div class="r23">
+            <div class="r24">
+                <div class="r25">
+                <div class="r26">
+                    <div>
+                        <span>Oferta especial</span>
+                    </div>
+                    <div class="r27">
+                    <span>Air Jordan edição de colecionador</span>
+                    </div>
+                    <div class="r271">
+                        <span>Lorem ipsum dolo sit amet, consectetur adipiscing elit, sed do<br>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</br> ad minim veniam, quis nostrud exercitation laboris nisi ut aliquip<br/></span>
+                    </div>
+                    <div class="r273">
+                    <button>Ver Oferta</button>
+                    </div>
+                </div>
+            </div>
+            <div class="imagem">
+                <div>
+                    <img src="tenis.png" alt="meal"></img>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+    </section>
+)
+}
