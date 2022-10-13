193915480d279a5c322cce2aa0245a7fecfd7e3b
SaorCamops <saor93@gmail.com>
2022-10-13 12:15:43 -0300

    resolvido o problema da home

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import Headphones from "./Pages/Categorias/Headphones/head.js";
 import Trabalhe from "./Pages/Categorias/Tenis/tenis.js";
 import Home from "./Pages/Home/index.js";
 
-
-
-
-
 export const AppContext = React.createContext();
 
 export default function App() {
@@ -19,17 +15,17 @@ export default function App() {
 
   return (
     <AppContext.Provider value={{ carrinho, setCarrinho }}>
-      <>
+      <React.Fragment>
         <Routes>
-          <Route path={'/'} element={<Navigate to={'./Pages/Home'}/>}/>
-          <Route path={'./Pages/Home'} element={Home()}/>
-          <Route path={'./Pages/Categorias/Bones'} element={Bones()}/>
-          <Route path={'./Pages/Categorias/Calcas'}element={Calcas()}/>
-          <Route path={'./Pages/Categorias/Camisetas'} element={Camisetas()} />
-          <Route path={'./Pages/Categorias/Headphones'} element={Headphones()}/>
-          <Route path={'./Pages/Categorias/Tenis'} element={Trabalhe()} />
+          <Route path={'/'} element={<Home/>}/>
+          <Route path={'/Pages/Home'} element={<Home/>}/>
+          <Route path={'/Pages/Categorias/Bones'} element={Bones()}/>
+          <Route path={'/Pages/Categorias/Calcas'}element={Calcas()}/>
+          <Route path={'/Pages/Categorias/Camisetas'} element={Camisetas()} />
+          <Route path={'/Pages/Categorias/Headphones'} element={Headphones()}/>
+          <Route path={'/Pages/Categorias/Tenis'} element={Trabalhe()} />
         </Routes>
-      </>
+      </React.Fragment>
     </AppContext.Provider>
   )
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 let Home = () => {
-    <React.Fragment>
-            <Header/>
-            <Footer/>
-    </React.Fragment>
+    return(
+        <React.Fragment>
+                <Header/>
+                <Footer/>
+        </React.Fragment>
+    );
 
 }
 export default Home;
