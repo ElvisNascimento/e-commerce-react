5729201ebc5ab664d351c242b59dc65de2a2ff58
mariolivera <mariooliveiratec@hotmail.com>
2022-10-14 15:43:53 -0300

    botoes vetorizados

--- src/components/Produto/Collection/Linkicons/Linkicons.js
+++ src/components/Produto/Collection/Linkicons/Linkicons.js
@@ -3,6 +3,9 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { Button } from '@mui/material';
 import FrameC from '../img/FrameC.png'
+import FrameCa from '../img/FrameCa.png'
+import FrameH from '../img/FrameH.png'
+import FrameS from '../img/FrameS.png'
 
 import './index.css'
 
@@ -21,10 +24,18 @@ export default function Linkicons(){
                     <div>
                         <Button className="botao"><Link to="/Camisetas"><p className="texto"><img src={FrameC}/>Camisetas</p></Link></Button>
                     </div>
-                    <Button className="botao"><Link to="/Calcas" ><p className="texto">Calças</p></Link></Button>
-                    <Button className="botao"><Link to="/Bones"><p className="texto">Bonés</p></Link></Button>
-                    <Button className="botao"><Link to="/Headphones"><p className="texto">Headphones</p></Link></Button>
-                    <Button className="botao"><Link to="/Tenis"><p className="texto">Tênis</p></Link></Button>
+                    <div>
+                        <Button className="botao"><Link to="/Calcas" ><p className="texto"><img src={FrameCa}/>Calças</p></Link></Button>
+                    </div>
+                    <div>
+                        <Button className="botao"><Link to="/Bones"><p className="texto"><img src={FrameCa}/>Bonés</p></Link></Button>
+                    </div>
+                    <div>
+                        <Button className="botao"><Link to="/Headphones"><p className="texto"><img src={FrameH}/>Headphones</p></Link></Button>
+                    </div>
+                    <div>
+                        <Button className="botao"><Link to="/Tenis"><p className="texto"><img src={FrameS}/>Tênis</p></Link></Button>
+                    </div>
                 </nav>
             </div>
         </section>
