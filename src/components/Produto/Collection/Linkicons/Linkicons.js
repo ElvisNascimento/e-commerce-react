f021562882fe091cd35f85e5974678ffb8fd9f03
mariolivera <mariooliveiratec@hotmail.com>
2022-10-14 13:17:13 -0300

    add funçao de retornar para pagina da home da calca e bones

--- src/components/Produto/Collection/Linkicons/Linkicons.js
+++ src/components/Produto/Collection/Linkicons/Linkicons.js
@@ -3,17 +3,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 
 
+
 export default function Linkicons(){
     return(
-        <div className="texto">
+        <div>
             <p>Coleções em destaque</p>
-            <nav>
-                <Link to="/Camisetas" >Camisetas</Link>
-                <Link to="/Calcas">Calças</Link>
-                <Link to="/Bones">Bonés</Link>
-                <Link to="/Headphones">Headphones</Link>
-                <Link to="/Tenis">Tênis</Link>
-            </nav>
+            <div>
+                <nav>
+                    <Link to="/Camisetas">Camisetas</Link>
+                    <Link to="/Calcas" > Calças</Link>
+                    <Link to="/Bones"> Bonés</Link>
+                    <Link to="/Headphones">Headphones</Link>
+                    <Link to="/Tenis">Tênis</Link>
+                </nav>
+            </div>
         </div>
     );
 }
--- src/components/Produtos/Calcas/index.js
+++ src/components/Produtos/Calcas/index.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+import { Button } from '@mui/material'
 export default function Calças(){
-    return(
+     return (
         <div>
             <h1>Pagina das Calças</h1>
+
+            <Link to='/'>
+                <Button variant='outlined' >Voltar pro inicio</Button>
+            </Link>
         </div>
+    
+
     )
 }
