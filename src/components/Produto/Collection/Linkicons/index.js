219027cd6961b4b9e4073f9b567b4e855c22b8ac
mariolivera <mariooliveiratec@hotmail.com>
2022-10-14 09:16:40 -0300

    css

--- src/components/Produto/Collection/Linkicons/index.js
+++ src/components/Produto/Collection/Linkicons/index.js
@@ -1,29 +1,24 @@
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
-//import {DirectionsCar, TwoWheeler, DirectionsBike} from "@mui/icons-material"
+//import "./styles.css"
+//import frame10 from "./imgcollection/imgicons/Frame 10.png"
 
-
-// function Informacao (props) {
-//     return (
-//         <Card style={{marginTop: 10}}>
-//             <CardContent>
-//             <DirectionsCar/>
-//             {props.icone}
-//             <strong style={{marginRight:'0 10px'}}> 
-//             {props.quantidade}</strong>
-
-//                 {props.titulo}
-//             </CardContent>
-//         </Card>
+// export default function imgicons(){
+//     return(
+//         <div>
+//             <img src={frame10} alt= "this is frame imagem"/>
+//         </div>
 //     )
 // }
+
+
 export default function Linkicons(){
     return(
-        <div>
+        <div >
             <p>Coleções em destaque</p>
             <nav>
-                <Link to="/Camisetas">Camisetas</Link>
+                <Link to="/Camisetas" >Camisetas</Link>
                 <Link to="/Calcas">Calças</Link>
                 <Link to="/Bones">Bonés</Link>
                 <Link to="/Headphones">Headphones</Link>
