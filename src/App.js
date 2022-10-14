4c306933a9ef88fc304bef21f95f02b41ba760ba
mariolivera <mariooliveiratec@hotmail.com>
2022-10-14 07:35:58 -0300

    rasolvido o caminho das trilhas de bonés e calças

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Linkicons from "./components/Produto/Collection/Linkicons"
 import Bones from "./components/Produtos/Bones/";
-import Calças from "./components/Produtos/Calcas";
+import Calcas from "./components/Produtos/Calcas";
 import Camisetas from "./components/Produtos/Camisetas";
 import Headphones from "./components/Produtos/Headphones";
 import Tenis from "./components/Produtos/Tenis/";
@@ -14,8 +14,8 @@ export default function App() {
                 <Linkicons/>
                     <Routes>
                         <Route path="camisetas" element={<Camisetas/>}/>
-                        <Route path="calças" element={<Calças/>}/>
-                        <Route path="bonês" element={<Bones/>}/>
+                        <Route path="calcas" element={<Calcas/>}/>
+                        <Route path="bones" element={<Bones/>}/>
                         <Route path="headphone" element={<Headphones/>}/>
                         <Route path="tenis" element={<Tenis/>}/>
                     </Routes>
