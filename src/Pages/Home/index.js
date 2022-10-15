e57571b82a989df5edbd420235422954050198d5
ElvisNascimento <elvisnascimentodev@gmail.com>
2022-10-15 16:27:04 -0300

    iniciei o carrosel e tentei arrumar o banner footer mas nao rolrou

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,18 +4,18 @@ import Footer from "../../components/Footer";
 import Collection from "../../components/Produto/Collection"
 import Linkicons from "../../components/Produto/Collection/Linkicons/Linkicons.js";
 import ProdutosAlta from "../../components/ProdutosAlta/produtosAlta";
+import BannerFooter from "../../components/BannerFooter";
 
 let Home = () => {
     return(
         <React.Fragment>
                 <Header/>
-
                 <ProdutosAlta/>
                 <Collection/>
-                <Linkicons/> 
+                <Linkicons/>
+                {/* <BannerFooter/> */}
                 <Footer/>
         </React.Fragment>
     );
-
 }
 export default Home;
---
+++ src/components/Carrosel/index.js
@@ -0,0 +1,7 @@
+export default function Carrosel() {
+    return (
+        <>
+            <h1>CARROSEL</h1>
+        </>
+    )
+}
