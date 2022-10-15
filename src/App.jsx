8c0b7725041647c5bf0c8a1feaa4d6b961b647ff
SaorCampos <saor93@gmail.com>
2022-10-15 13:25:37 +0000

    arrumei os problemas em produtos em alta

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Camisetas from "./components/Produtos/Camisetas";
 import Headphones from "./components/Produtos/Headphones";
 import Tenis from "./components/Produtos/Tenis/";
 import Login from "./Pages/Login/index.js";
+import Cadastro from "./Pages/Cadastro/index.jsx";
 
 export const AppContext = React.createContext();
 
@@ -26,11 +27,12 @@ export default function App() {
           <Route path={'/Pages/Home'} element={<Home/>}/>
           <Route path={'/Pages/Login'} element={<Login/>}/>
           {/* <Route path={'/Pages/Produtos'} element={<TodosProdutos/>}/> */}
-          <Route path="camisetas" element={<Camisetas/>}/>
-          <Route path="calcas" element={<Calcas/>}/>
-          <Route path="bones" element={<Bones/>}/>
-          <Route path="headphones" element={<Headphones/>}/>
-          <Route path="tenis" element={<Tenis/>}/>
+          <Route path="/camisetas" element={<Camisetas/>}/>
+          <Route path="/calcas" element={<Calcas/>}/>
+          <Route path="/bones" element={<Bones/>}/>
+          <Route path="/headphones" element={<Headphones/>}/>
+          <Route path="/tenis" element={<Tenis/>}/>
+          <Route path='/cadastro' element={<Cadastro/>}/>
         
         </Routes>
           
--- src/Pages/Cadastro/index.jsx
+++ src/Pages/Cadastro/index.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import Log from "../../components/Log";
 
 export default function Cadastro(){
 
     return(
         <React.Fragment>
-
+            <Log/>
+            
             <Footer/>
         </React.Fragment>
     )
