4af05c012ddaab4d6efad9dd05fb0afe87546cdc
mariolivera <mariooliveiratec@hotmail.com>
2022-10-15 17:13:32 +0000

    estilo botoes finalizado

--- src/components/Produto/Collection/Linkicons/Linkicons.js
+++ src/components/Produto/Collection/Linkicons/Linkicons.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
-import { Button } from '@mui/material';
+import { Button} from '@mui/material';
 import FrameC from '../img/FrameC.png'
 import FrameCa from '../img/FrameCa.png'
 import FrameH from '../img/FrameH.png'
@@ -17,24 +17,24 @@ export default function Linkicons(){
             <div className="cont">
                 <nav className='d-flex d-flex-row-row justify-content-center'>
                     <Link to="/"></Link>
-                    <div>
-                        <Button className="botao"><Link to="/Camisetas"><img src={FrameC}/></Link></Button>
+                    <div className="botao">
+                        <Button className="botao1"><Link to="/Camisetas"><img src={FrameC}/></Link></Button>
                         <p className="texto">Camisetas</p>
                     </div>
-                    <div>   
-                        <Button className="botao"><Link to="/Calcas" ><img src={FrameCa}/></Link></Button>
+                    <div className="botao">   
+                        <Button className="botao1"><Link to="/Calcas" ><img src={FrameCa}/></Link></Button>
                         <p className="texto">Calças</p>
                     </div>
-                    <div>
-                        <Button className="botao"><Link to="/Bones"><img src={FrameCa}/></Link></Button>
+                    <div className="botao">
+                        <Button className="botao1"><Link to="/Bones"><img src={FrameCa}/></Link></Button>
                         <p className="texto">Bonés</p>
                     </div>
-                    <div>
-                        <Button className="botao"><Link to="/Headphones"><img src={FrameH}/></Link></Button>
+                    <div className="botao">
+                        <Button className="botao1"><Link to="/Headphones"><img src={FrameH}/></Link></Button>
                         <p className="texto">Headphones</p>
                     </div>
-                    <div>
-                        <Button className="botao"><Link to="/Tenis"><img src={FrameS}/></Link></Button>
+                    <div className="botao">
+                        <Button className="botao1"><Link to="/Tenis"><img src={FrameS}/></Link></Button>
                         <p className="texto">Tênis</p>
                     </div>
                 </nav>
