0bf64562c7e786a967af9a7759954aeb1b8e6c07
SaorCampos <saor93@gmail.com>
2022-10-15 20:34:25 +0000

    terminei a pagina de cadastro

--- src/Pages/Cadastro/index.jsx
+++ src/Pages/Cadastro/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Footer from "../../components/Footer";
-import Logo from '../../img/LogoDigital.png'
-import './style.css'
+import Logo from '../../img/LogoDigital.png';
+import './style.css';
+import { Checkbox } from "@mui/material";
+import { pink } from '@mui/material/colors';
 
 export default function Cadastro(){
 
@@ -23,24 +25,56 @@ export default function Cadastro(){
                             <hr className="quebra" />
                         </div>
                         <div className="form-group">
-                            <label for="nome" className="labelNome">Nome Completo*</label>
-                            <input  required  pattern="[A-Za-z]" type="text" className="form-control inputNome" id="nome" placeholder="Insira seu nome" />
+                            <label htmlFor="nome" className="labelNome">Nome Completo*</label>
+                            <input  required  pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" type="text" className="form-control inputNome" id="nome" placeholder="Insira seu nome" />
                         </div>
                         <div className="form-group">
-                            <label for="cpf" className="labelCpf">CPF*</label>
+                            <label htmlFor="cpf" className="labelCpf">CPF*</label>
                             <input required  pattern="[0-9]{11}" type="text" className="form-control inputCpf" id="cpf" placeholder="Insira seu CPF" />
                         </div>
                         <div className="form-group">
-                            <label for="email" className="labelEmail">E-mail*</label>
+                            <label htmlFor="email" className="labelEmail">E-mail*</label>
                             <input required type="email" className="form-control inputEmail" id="email" placeholder="Insira seu E-mail" />
                         </div>
                         <div className="form-group">
-                            <label for="celular" className="labelCelular">Celular*</label>
+                            <label htmlFor="celular" className="labelCelular">Celular*</label>
                             <input required type="text" pattern="[0-9]{11}" className="form-control inputCelular" id="celular" placeholder="Insira seu celular" />
                         </div>
                     </div>
+                    <div className="form-group formu2">
+                        <div className="form-group">
+                            <span className="infoE">Informações de Entrega</span>
+                            <hr className="quebra" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="endereco" className="labelEndereco">Endereço*</label>
+                            <input required pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" type="text" className="form-control inputEndereco" id="endereco" placeholder="Insira seu endereço" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="bairro" className="labelBairro">Bairro*</label>
+                            <input required pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" type="text" className="form-control inputBairro" id="endereco" placeholder="Insira seu bairro" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="cidade" className="labelCidade">Cidade*</label>
+                            <input required pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" type="text" className="form-control inputCidade" id="cidade" placeholder="Insira sua cidade" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="cep" className="labelCep">CEP*</label>
+                            <input required pattern="[0-9]{8}" type="text" className="form-control inputCep" id="cep" placeholder="Insira seu CEP" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="complemento" className="labelComp">Complemento</label>
+                            <input pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" type="text" className="form-control inputComp" id="complemento" placeholder="Insira seu complemento" />
+                        </div>
+                    </div>
+                    <div className="form-check check">
+                        <input type="checkbox" className="cBox" id="exampleCheck1"/>
+                        <label className="cBoxLabel" htmlFor="exampleCheck1">Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente.</label>
+                    </div>
+                    <div className="btnCad">
+                        <button className="btnConf">Criar Conta</button>
+                    </div>
                 </form>
-                
                 <Footer/>
             </div>
         </React.Fragment>
