d0c2ccde3957a26be5286b2df87f565daca15525
Elias Fernandes <eliasneto0014@gmail.com>
2022-10-16 08:57:03 -0300

    criando as pastas DetalhesProdutos

---
+++ src/Pages/DetalhesProduto/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+
+export default function DetalhesProduto() {
+    return(
+        <>
+        <Header/>
+
+
+        <Footer/>
+        </>
+    )
+}
