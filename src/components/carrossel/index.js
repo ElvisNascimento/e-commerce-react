2320af63831eb149fd7335a88a89d023e8d93c5a
Natan Ferreira <natan_ael@hotmail.com>
2022-10-16 16:04:56 +0000

    concluindo algumas coisas

--- src/components/carrossel/index.js
+++ src/components/carrossel/index.js
@@ -1,27 +1,52 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
 import tenis from "./img/tenis.png"
+import './style.css';
 
 
 export default function Carrossel() {
     return (
+    
         <React.Fragment>
-        <div id="carouselExampleIndicators" className="carousel slide" dataRide="carousel">
+       
+       <Carousel className="carousel-principal">
+       <div id="carouselExampleIndicators" className="carousel slide" dataRide="carousel">
         <ol className="carousel-indicators">
-          <li dataTarget="#carouselExampleIndicators" dataSlideTo="0" className="active"></li>
-          <li dataTarget="#carouselExampleIndicators" dataSlideTo="1"></li>
-          <li dataTarget="#carouselExampleIndicators" dataSlideTo="2"></li>
+          <li dataTarget="#carouselExampleIndicators" dataSlideTo="0" className="active slideprimeiro"></li>
+          <li dataTarget="#carouselExampleIndicators" dataSlideTo="1" className="active"></li>
+          <li dataTarget="#carouselExampleIndicators" dataSlideTo="2" className="active"></li>
         </ol>
         <div className="carousel-inner">
+          
           <div className="carousel-item active">
-            <img className="d-block w-50 col md-20" src={tenis} alt="First slide"/>
+           
+          <div>
+            <div className="mx-auto text-center d-flex">
+             <div><h4>Melhores ofertas personalisadas</h4>
+              <h1>Queima de Estoque Nike</h1>
+              <p>consequat culpa exercitation mollit nisi excepteur do tempor laboris eiusmod irure cosectetur.</p>
+              <button className="botaoofertas">Ver ofertas</button>
+              </div> 
+            <div> <img className=" w-10 col md-10" src={tenis} alt="First slide" /></div>
+           
+             </div>
+            </div>  
+          
           </div>
-          <div className="carousel-item">
-            {/* {<img className="d-block w-100" src={tenis} alt="Second slide"/> } */}
+         
+          <div className="carousel-item " >
+      
+          <img className="d-block w-100" src={tenis} alt="Second slide"/> 
+          
           </div>
-          <div className="carousel-item">
-            {/* <img className="d-block w-100" src="./../components/carrossel/tenis.png" alt="Third slide"/> */}
+          
+          <div className="carousel-item ">
+          
+           <img className="d-block w-100" src={tenis} alt="Third slide"/>
+          
           </div>
         </div>
+        
         <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="sr-only">Previous</span>
@@ -30,7 +55,9 @@ export default function Carrossel() {
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="sr-only">Next</span>
         </a>
-      </div>
+      </div>  
+      </Carousel>
       </React.Fragment>
     );
-  }
+    }
+ 
