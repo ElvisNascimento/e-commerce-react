4d9b1f49aa3b99935649fe08206ab0ea6a9ec8a3
Natan Ferreira <natan_ael@hotmail.com>
2022-10-28 14:58:13 +0000

    concluindo carrossel

--- src/components/carrossel/index.js
+++ src/components/carrossel/index.js
@@ -1,49 +1,126 @@
-
- 
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from "react-router-dom"
 import Carousel from 'react-bootstrap/Carousel';
 import tenis from "./img/tenis.png"
+import bola from "./img/bola.png"
 import "./style.css"
 
 export default function Carrossel() {
+  return (
+    <Carousel className="container carousel-principal">
+      <Carousel.Item className="divisao">
+        <div className="linha">
+          <img className="circle" src={bola} alt="circle" />
+          <img className="nike" src={tenis} alt="nike" />
+        </div>
+        <div>
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div>
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+            <div>
+              <h2 className='texto2'> Queima de estoque Nike 🔥</h2>
+            </div>
+            <div>
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <Link to="/produtos">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </Link>
+            </div>
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
 
-  const [index, setIndex] = useState(0);
+      <Carousel.Item>
+        <div  className="linha">
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-    return (
-        <React.Fragment>
-          <Carousel style={{backgroundColor: "#ffffff"}} activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-              <div div class="d-flex">Melhores Ofertas Personalizadas
-                <h1><strong>Queima de estoque Nike</strong></h1>
-                <p>Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.</p>
-                <button style={{backgroundColor:"#f5f5f5"}}>Ver Ofertas</button>
-              <img className="d-block w-50" src={tenis} alt="First slide" />
-              </div>
-              <Carousel.Caption>
-                
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block w-50" src={tenis} alt="Second slide" />
-              <Carousel.Caption>
-                
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className="d-block w-50" src={tenis} alt="Third slide" />
-              <Carousel.Caption>
-               
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-      </React.Fragment>
-    );
-  }
-          
-          
-       
-          
-         
+          <img className="circle" src={bola} alt="circle" />
+          <img className="nike" src={tenis} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div>
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div>
+              <h2 className='texto2'>Queima de estoque Nike 🔥</h2>
+            </div>
+            <div>
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div className="linha">
+
+          <img className="circle" src={bola} alt="circle" />
+          <img className="nike" src={tenis} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div className="wrapper-texto1">
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div>
+              <h2 className='texto2'>Queima de estoque Nike 🔥</h2>
+            </div>
+            <div>
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div className="linha">
+
+          <img className="circle" src={bola} alt="circle" />
+          <img className="nike" src={tenis} alt="nike" />
+        </div>
+
+        <Carousel.Caption className="esquerda">
+          <div className="textos">
+            <div>
+              <p className="texto1"> Melhores ofertas personalizadas</p>
+            </div>
+
+            <div>
+              <h2 className='texto2'>
+                Queima de estoque Nike 🔥
+              </h2>
+            </div>
+            <div>
+              <p className="texto3">Consequat culpa exercitation mollit nisi excepteur do <br /> do tempor laboris eiusmod irure consectetur</p>
+            </div>
+            <div className="wrapper-button1">
+              <a href="">
+                <button className="buttonofertas">Ver Ofertas</button>
+              </a>
+            </div>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+    </Carousel>
+  );
+}
